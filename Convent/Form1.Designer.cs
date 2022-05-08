7b5372c0400f365352055072e99cae97bac2ef49
IDHSRG <aidin.serg@gmail.com>
2022-05-08 15:42:48 +0300

    Проверка
    Тестировка

--- Convent/Form1.Designer.cs
+++ Convent/Form1.Designer.cs
@@ -43,6 +43,8 @@ namespace Convent
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.ss10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ss2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -56,12 +58,13 @@ namespace Convent
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(201, 39);
+            this.button1.Location = new System.Drawing.Point(201, 42);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 27);
+            this.button1.Size = new System.Drawing.Size(106, 24);
             this.button1.TabIndex = 1;
             this.button1.Text = "10 =>2";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -158,11 +161,31 @@ namespace Convent
             this.ss2.Name = "ss2";
             this.ss2.Width = 210;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ввод";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(385, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Вывод";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(514, 361);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.textBox2);
@@ -195,6 +218,8 @@ namespace Convent
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn ss10;
         private System.Windows.Forms.DataGridViewTextBoxColumn ss2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
