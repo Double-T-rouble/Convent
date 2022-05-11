8adc70b1ee085d393eee933c9709697e2bec39a1
IDHSRG <aidin.serg@gmail.com>
2022-05-11 12:33:03 +0300

    Поле ввода и кнопки
    Настроил ввод числа в поле ввода, а также реакцию кнопок на введенное значение (а также сообщение об ошибке в случае недопустимого формата)

--- Convent/Form1.Designer.cs
+++ Convent/Form1.Designer.cs
@@ -43,6 +43,7 @@ namespace Convent
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -53,6 +54,7 @@ namespace Convent
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(168, 20);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
@@ -60,7 +62,7 @@ namespace Convent
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(106, 24);
             this.button1.TabIndex = 1;
-            this.button1.Text = "10 =>2";
+            this.button1.Text = "10 => 2";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -72,6 +74,7 @@ namespace Convent
             this.button2.TabIndex = 2;
             this.button2.Text = "2 => 10";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox2
             // 
@@ -164,11 +167,23 @@ namespace Convent
             this.label2.TabIndex = 7;
             this.label2.Text = "Вывод";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(54, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Это не число";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(514, 361);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
@@ -203,6 +218,7 @@ namespace Convent
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Convent/Form1.cs
+++ Convent/Form1.cs
@@ -20,12 +20,19 @@ namespace Convent
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            table.Columns.Add("10-я система", typeof(int));
-            table.Columns.Add("2-я система", typeof(int));
+            table.Columns.Add("10-я система", typeof(double));
+            table.Columns.Add("2-я система", typeof(double));
             dataGridView1.AllowUserToAddRows = false;
             dataGridView1.DataSource = table;
             dataGridView1.Columns[0].Width = 160;
             dataGridView1.Columns[1].Width = 197;
+
+            button1.Enabled = false;
+            button2.Enabled = false;
+
+            textBox2.Enabled = false;
+
+            label3.Visible = false;
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -53,6 +60,37 @@ namespace Convent
         {
             AboutProg APform = new AboutProg();
             APform.Show();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            double n;
+            if (double.TryParse(textBox1.Text, out n))
+            {
+                label3.Visible = false;
+                if (textBox1.Text.Contains('2') || textBox1.Text.Contains('3') || textBox1.Text.Contains('4') || textBox1.Text.Contains('5')
+                    || textBox1.Text.Contains('6') || textBox1.Text.Contains('7') || textBox1.Text.Contains('8') || textBox1.Text.Contains('9'))
+                {
+                    button2.Enabled = true;
+                    button1.Enabled = false;
+                }
+                else
+                {
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                }
+            }
+            else
+            {
+                label3.Visible = true;
+                button1.Enabled = false;
+                button2.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
