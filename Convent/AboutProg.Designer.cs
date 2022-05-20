bb2a009afceb3fb9274e6d3a23b72adb0e67828c
nicopo <100519498+lnicopol@users.noreply.github.com>
2022-05-20 13:40:30 +0300

    1

--- Convent/AboutProg.Designer.cs
+++ Convent/AboutProg.Designer.cs
@@ -38,6 +38,8 @@ namespace Convent
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -108,7 +110,7 @@ namespace Convent
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(22, 433);
+            this.label7.Location = new System.Drawing.Point(22, 441);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(330, 20);
             this.label7.TabIndex = 7;
@@ -119,18 +121,41 @@ namespace Convent
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label8.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.label8.Location = new System.Drawing.Point(102, 461);
+            this.label8.Location = new System.Drawing.Point(90, 461);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(164, 20);
             this.label8.TabIndex = 8;
-            this.label8.Text = "aidin.serg@gmail.com";
+            this.label8.Text = "aidin.serg@gmail.com\r\n";
             this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(111, 386);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(147, 20);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Ссылка на GitHub:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label10.Location = new System.Drawing.Point(33, 410);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(316, 20);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "https://github.com/Double-T-rouble/Convent";
             // 
             // AboutProg
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(364, 490);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -157,5 +182,7 @@ namespace Convent
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
