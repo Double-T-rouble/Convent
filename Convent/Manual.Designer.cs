19ece39449426402eb302542101b345e6a25caf6
nicopo <100519498+lnicopol@users.noreply.github.com>
2022-05-21 15:07:16 +0300

    1

--- Convent/Manual.Designer.cs
+++ Convent/Manual.Designer.cs
@@ -140,7 +140,7 @@ namespace Convent
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label4.Location = new System.Drawing.Point(368, 32);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(335, 16);
+            this.label4.Size = new System.Drawing.Size(334, 16);
             this.label4.TabIndex = 9;
             this.label4.Text = "(Или используйте сочетания \"Alt+Up\" и \"Alt+Down\")";
             // 
